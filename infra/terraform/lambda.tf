69d87bc5bd0c586575cc61ce19561ad89d39a869
fazelshah <fazel.shah@cryptlex.com>
2026-01-14 10:19:18 +0530

    added code

---
+++ infra/terraform/lambda.tf
@@ -0,0 +1,19 @@
+resource "aws_lambda_function" "uptime_checker" {
+  function_name = "uptime-checker"
+  runtime       = "nodejs18.x"
+  handler       = "handler.handler"
+  role          = aws_iam_role.lambda_role.arn
+
+  filename         = "../../lambda.zip"
+  source_code_hash = filebase64sha256("../../lambda.zip")
+
+  environment {
+    variables = {
+      DB_HOST        = aws_db_instance.uptime_db.address
+      DB_NAME        = "uptime"
+      DB_USER        = "uptime"
+      DB_PASSWORD    = var.db_password
+      SNS_TOPIC_ARN  = aws_sns_topic.alerts.arn
+    }
+  }
+}
